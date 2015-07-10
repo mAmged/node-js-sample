be34aa88f1e008b89430f97ebfe0fb0b0a0c5207
mAmged <m.amged@ymail.com>
2015-07-10 18:00:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Email: m.amged (@) hotmail.com')
+  response.send('m.amged (@) hotmail.com')
 })
 
 app.listen(app.get('port'), function() {
