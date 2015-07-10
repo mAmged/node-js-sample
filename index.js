526e090039f5aed24fb6399d8d53cd3a3c5582ce
mAmged <m.amged@ymail.com>
2015-07-10 23:14:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('m.amged (@) hotmail.com')
+  response.send('<html><head><title>MEGO SEO TEST</title><meta name="description" content="155 characters of message matching text with a call to action goes here"></head><body>m.amged (@) hotmail.com</body>')
 })
 
 app.listen(app.get('port'), function() {
